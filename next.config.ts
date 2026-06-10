07235fdf3b3c977464d0f480f973c4294e96cc72
Emre Tarhan <emretarhan5@gmail.com>
2026-06-10 12:01:33 +0300

    webpack-symlink-fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["@supabase/supabase-js"],
   poweredByHeader: false,
   reactStrictMode: true,
+  webpack: (config) => {
+    // node_modules 2 symlink sorununu önle
+    config.resolve.symlinks = false;
+    return config;
+  },
 };
 
 export default nextConfig;
